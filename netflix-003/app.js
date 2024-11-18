baa7ee242cdb847cb83bb1830021d155fcf77d21
Samuel <samuelbermudogarcia@gmail.com>
2024-11-18 12:48:56 +0100

    netflixx microservicios 02

--- netflix-003/app.js
+++ netflix-003/app.js
@@ -11,3 +11,7 @@ app.get("/compras", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
+
+//50.19.165.25:3000/api
+//100.29.42.210:3001/api   
+//3.87.192.117:3002/api
